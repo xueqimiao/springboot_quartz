4dd185121b00304517e81f90088828fbc28a9e94
yeasin <yeasin@qq.com>
2019-12-02 15:28:31 +0800

    xueqimiao

---
+++ springboot_quartz/src/main/java/com/xueqimiao/TestQuartz/Test01.java
@@ -0,0 +1,20 @@
+package com.xueqimiao.TestQuartz;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@EnableScheduling
+public class Test01 {
+
+    //3.添加定时任务
+    @Scheduled(cron = "0/5 * * * * ?")
+    //或直接指定时间间隔，例如：5秒
+    //@Scheduled(fixedRate=5000)
+    private void configureTasks() {
+        System.err.println("执行静态定时任务时间: " + new Date());
+    }
+}
